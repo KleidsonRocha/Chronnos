67f44d5d8b047f6fc9420217b2d08669b6e9f525
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2024-04-18 22:03:09 -0300

    Adicao do feedback haptico ao componente ChronnosButton.jsx

--- FrontEnd/src/Components/inputs-buttons/ChronnosButton/ChronnosButton.jsx
+++ FrontEnd/src/Components/inputs-buttons/ChronnosButton/ChronnosButton.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import './styles.css';
 
 const ChronnosButton = ({ onClick, className, children }) => {
+  const feedbackHapticoButton = () => {
+    if ("vibrate" in navigator) navigator.vibrate(30);
+  }
+
+  const handleClick = () => {
+    if (onClick) onClick();
+    feedbackHapticoButton();
+  }
+
   return (
-    <button onClick={onClick} className={className}>
+    <button onClick={handleClick} className={className}>
       {children}
     </button>
   );
