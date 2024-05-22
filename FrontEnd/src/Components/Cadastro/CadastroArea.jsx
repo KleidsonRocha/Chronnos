e94ac4a174799580c82a2f03214d50365f5732bd
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2024-05-22 01:41:00 -0300

    Transicao dos componentes da pagina de cadastro de area

--- FrontEnd/src/Components/Cadastro/CadastroArea.jsx
+++ FrontEnd/src/Components/Cadastro/CadastroArea.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useGlobalContext } from '../../App';
+import MainMobile from '../layouts/MainMobile/MainMobile';
+import ChronnosInput from '../inputs-buttons/ChronnosInput/ChronnosInput';
+import ChronnosButton from '../inputs-buttons/ChronnosButton/ChronnosButton';
 
 const CadastroArea = () => {
   const { RotaBanco } = useGlobalContext();
   const [nomeArea, setNomeArea] = useState('');
   const [cor, setCor] = useState('');
-  const [idUsuario, setIdUsuario] = useState(null); 
+  const [idUsuario, setIdUsuario] = useState(null);
 
   // Efeito para obter o ID do usuário do cookie quando o componente é montado
   useEffect(() => {
@@ -33,7 +36,7 @@ const CadastroArea = () => {
     };
 
     try {
-      const response = await fetch( RotaBanco +'/usuarios/adicionarArea', {
+      const response = await fetch(RotaBanco + '/usuarios/adicionarArea', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -52,22 +55,12 @@ const CadastroArea = () => {
   };
 
   return (
-    <div>
-      <h1>Adicionar Nova Área</h1>
-      <input
-        type="text"
-        placeholder="Nome da Área"
-        value={nomeArea}
-        onChange={(e) => setNomeArea(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Cor"
-        value={cor}
-        onChange={(e) => setCor(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Adicionar Área</button>
-    </div>
+    <MainMobile className="main-mob-cent">
+      <h1>Cadastro de área</h1>
+      <ChronnosInput className="input-default" type="text" placeholder="Nome da Área" value={nomeArea} onChange={(e) => setNomeArea(e.target.value)}></ChronnosInput>
+      <ChronnosInput className="input-default" type="text" placeholder="Cor" value={cor} onChange={(e) => setCor(e.target.value)}></ChronnosInput>
+      <ChronnosButton className="button-default" onClick={handleSubmit}>Adicionar Área</ChronnosButton> 
+    </MainMobile>
   );
 };
 
