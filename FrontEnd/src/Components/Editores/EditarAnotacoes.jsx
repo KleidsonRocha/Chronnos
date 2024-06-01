1a9e3c2f1b4083e6f737475a46122ee129cc04f5
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2024-05-31 23:07:48 -0300

    Padronizacao da pagina de edicao de anotacoes

--- FrontEnd/src/Components/Editores/EditarAnotacoes.jsx
+++ FrontEnd/src/Components/Editores/EditarAnotacoes.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useGlobalContext } from '../../App';
+import MainMobile from '../layouts/MainMobile/MainMobile';
+import ChronnosInput from '../inputs-buttons/ChronnosInput/ChronnosInput';
+import ChronnosButton from '../inputs-buttons/ChronnosButton/ChronnosButton';
 import Dock from '../dock/Dock';
+import { useHref } from 'react-router-dom';
 
 const EditarAnotacoes = () => {
     const { RotaBanco } = useGlobalContext();
@@ -59,15 +63,18 @@ const EditarAnotacoes = () => {
 
     return (
         <>
-            {curso && (
-                <form id="curso-formulario">
-                    <label htmlFor="anotacoes">Anotações:</label>
-                    <textarea id="anotacoes" name="anotacoes" value={curso.ANOTACOES} onChange={(e) => handleInputChange(e, 'ANOTACOES')}></textarea><br />
-                    <br />
-                </form>
-            )}
-            <button id="editar-curso-btn" onClick={salvarAlteracoes}>Salvar Alterações</button>
-            <Dock/>
+            <MainMobile className="form-mob-cent">
+                {curso && (
+                    <form id="curso-formulario">
+                        <div className="layout-vertical">
+                            <h1>Editar uma anotação</h1>
+                            <ChronnosInput type="text" className="input-default" placeholder="Link do Curso" value={curso.ANOTACOES} onChange={(e) => handleInputChange(e, 'ANOTACOES')}></ChronnosInput>
+                            <ChronnosButton id="editar-curso-btn" className="button-default" onClick={salvarAlteracoes}>Salvar alterações</ChronnosButton>
+                        </div>
+                    </form>
+                )}
+            </MainMobile>
+            <Dock />
         </>
     );
 };
