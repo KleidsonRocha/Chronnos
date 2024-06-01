3e5d15f988f1a08834d2b9c2ee82fa03f7f7a249
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2024-06-01 00:06:47 -0300

    Teste inicial do sistema de confeti nos popups

--- FrontEnd/src/Components/ChronnosPopUp/ChronnosPopUp.jsx
+++ FrontEnd/src/Components/ChronnosPopUp/ChronnosPopUp.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles.css';
 import ChronnosButton from '../inputs-buttons/ChronnosButton/ChronnosButton';
+import confetti from 'canvas-confetti';
 
-const ChronnosPopUp = ({ title, btntxt, btntype, cmd }) => {
+const ChronnosPopUp = ({ title, btntxt, btntype, cmd, conft = false }) => {
+    useEffect(() => {
+        if (conft) {
+            confetti({
+                particleCount: 69,
+                spread: 50,
+                origin: { y: 0.52 },
+              });
+        }
+    }, [conft]);
+
     return (
         <div className="popup">
             <p className="txt-titulo">{title}</p>
