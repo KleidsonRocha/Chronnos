0f921fe4899f57ba20ddce30d951551e33d93279
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2024-06-08 19:20:25 -0300

    Implementando o formato novo do grafico na pagina de timeline compartilhada

--- FrontEnd/src/Components/Compartilhar.jsx
+++ FrontEnd/src/Components/Compartilhar.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import "../Assets/utility.css";
-import "../Components/timeline/styles.css"
+import "../Components/timeline/styles.css";
 import MainMobile from './layouts/MainMobile/MainMobile';
 import Dock from './dock/Dock';
 import { useGlobalContext } from '../App';
 import ChronnosTitleInput from './inputs-buttons/ChronnosTitleInput/ChronnosTitleInput';
 import Chart from 'chart.js/auto';
 
-
 const Compartilhar = () => {
   const [cursos, setCursos] = useState([]);
-  const [nome, setNome] = useState(null);;
-  const [email, setEmail] = useState(null);;
+  const [nome, setNome] = useState(null);
+  const [email, setEmail] = useState(null);
 
   const { RotaBanco } = useGlobalContext();
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const usuarioId = urlParams.get('USUARIO_ID');
@@ -26,7 +26,6 @@ const Compartilhar = () => {
           console.log(usuarioId);
           window.location.href = '/Login';
           throw new Error('ID do usuário não encontrado no cookie');
-          //pop-up de erro necessário
         }
 
         const response = await fetch(RotaBanco + `/usuarios/listarCursosDoUsuario?usuario_id=${usuarioId}`);
@@ -60,13 +59,10 @@ const Compartilhar = () => {
             .catch(error => ({ ...curso, PAGAMENTO_NOME: 'Erro ao obter detalhes de pagamento' }))
         );
 
-
-        // Esperar todas as promessas de área e matéria serem resolvidas ou rejeitadas
         const areasResultados = await Promise.allSettled(areasPromises);
         const materiasResultados = await Promise.allSettled(materiasPromises);
         const pagamentoResultados = await Promise.allSettled(pagamentoPromises);
 
-        // Consolidar os resultados
         const cursosCompleto = cursos.map((curso, index) => ({
           ...curso,
           AREA_NOME: areasResultados[index].status === 'fulfilled' ? areasResultados[index].value.AREA_NOME : areasResultados[index].reason,
@@ -109,47 +105,71 @@ const Compartilhar = () => {
   };
 
   const renderizarGrafico = () => {
-    const ctx = document.getElementById('graficoPizza');
-    const valores = cursos.map(curso => curso.VALOR);
-    const nomes = cursos.map(curso => curso.NOME);
-    const cores = cursos.map(curso => curso.AREA_COR);
+    const ctx = document.getElementById('graficoPizza').getContext('2d');
+    if (window.myChart) {
+      window.myChart.destroy();
+    }
 
-    new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: nomes,
-        datasets: [{
-          label: 'Valor por Cursos',
-          data: valores,
-          backgroundColor: cores,
-        }]
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-          title: {
-            display: true,
-            text: 'Valor por Cursos'
+    const areaCount = cursos.reduce((acc, curso) => {
+      acc[curso.AREA_NOME] = (acc[curso.AREA_NOME] || 0) + 1;
+      return acc;
+    }, {});
+
+    const chartData = {
+      labels: Object.keys(areaCount),
+      datasets: [{
+        label: 'Cursos pertencentes à área',
+        data: Object.values(areaCount),
+        backgroundColor: cursos.map(curso => curso.AREA_COR),
+      }]
+    };
+
+    const chartOptions = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'bottom',
+          labels: {
+            font: {
+              size: 16,
+              family: 'Inter',
+              style: 'normal',
+              weight: 'normal',
+            },
+            color: '#000000',
           }
+        },
+        title: {
+          display: true,
+          text: 'Total de cursos por áreas',
+          font: {
+            size: 20,
+            family: 'Inter',
+            style: 'normal',
+            weight: 'bold'
+          },
+          color: '#000000',
+          align: 'left',
         }
       }
+    };
+
+    window.myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: chartData,
+      options: chartOptions,
     });
   };
-
 
   return (
     <>
       <MainMobile className={"main-mob"}>
         <ChronnosTitleInput title={nome} format="bold"></ChronnosTitleInput>
-        <ChronnosTitleInput title={email} format="bold"></ChronnosTitleInput>
         <div className="holder-timeline-graf">
           <div className="frame-timeline">
             {cursosOrdenados.map(curso => (
-              <a href={`/VisualizarCursoCompartilhado?ID_CURSO=${curso.ID_CURSO}`}>
-                <button key={curso.ID_CURSO} className="tab-timline" style={{ borderLeft: `2px solid ${curso.AREA_COR}` }}>
+              <a href={`/VisualizarCursoCompartilhado?ID_CURSO=${curso.ID_CURSO}`} key={curso.ID_CURSO}>
+                <button className="tab-timline" style={{ borderLeft: `2px solid ${curso.AREA_COR}` }}>
                   <h1>{curso.NOME}</h1>
                   <p>{formatarData(curso.DATA_FINI)}</p>
                 </button>
@@ -157,11 +177,13 @@ const Compartilhar = () => {
             ))}
           </div>
           <div className="frame-grafico">
-            <canvas id="graficoPizza"></canvas>
+            <div className="canvas-graf">
+              <canvas id="graficoPizza"></canvas>
+            </div>
           </div>
         </div>
       </MainMobile>
-      <Dock></Dock>
+      <Dock/>
     </>
   );
 };
