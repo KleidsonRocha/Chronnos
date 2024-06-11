ced979cee5a3f9af95154b818cdb55bf8023b8b9
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2024-06-11 04:55:16 -0300

    Adicionando o formato padrao de estilo ao botao de troca de arquivo na pagina de edicao de curso

--- FrontEnd/src/Components/Editores/EditarCurso.jsx
+++ FrontEnd/src/Components/Editores/EditarCurso.jsx
@@ -15,12 +15,13 @@ const EditarCurso = () => {
   const [areasDoUsuario, setAreasDoUsuario] = useState([]);
   const [showPopup, setShowPopup] = useState(false); // Estado para controlar a exibição do pop-up
   const [showPopupEdicao, setShowPopupEdicao] = useState(false); // Estado para controlar a exibição do pop-up
+  const [nomeArq, setNomeArq] = useState('');
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const cursoId = urlParams.get('ID_CURSO');
     const url = RotaBanco + `/curso/listarCursoEspecifico?cursoId=${cursoId}`;
-    
+
     fetch(url)
       .then(response => {
         if (!response.ok) {
@@ -184,6 +185,12 @@ const EditarCurso = () => {
             />
           </div>
           <input type="file" id="imagem" name="imagem" onChange={handleImagemChange} />
+          <label for="imagem" className="trocar-arquivo">
+            <p>{nomeArq ? `${nomeArq}` : 'Substituir o certificado'}</p>
+            <svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2.69004 24C2.03004 24 1.46504 23.765 0.995039 23.295C0.525039 22.825 0.290039 22.26 0.290039 21.6V2.4C0.290039 1.74 0.525039 1.175 0.995039 0.705C1.46504 0.235 2.03004 0 2.69004 0H11.3C11.62 0 11.925 0.06 12.215 0.18C12.505 0.3 12.76 0.47 12.98 0.69L18.8 6.51C19.02 6.73 19.19 6.985 19.31 7.275C19.43 7.565 19.49 7.87 19.49 8.19V13.2C19.49 13.54 19.375 13.825 19.145 14.055C18.915 14.285 18.63 14.4 18.29 14.4C17.95 14.4 17.665 14.285 17.435 14.055C17.205 13.825 17.09 13.54 17.09 13.2V8.4H12.29C11.95 8.4 11.665 8.285 11.435 8.055C11.205 7.825 11.09 7.54 11.09 7.2V2.4H2.69004V21.6H12.29C12.63 21.6 12.915 21.715 13.145 21.945C13.375 22.175 13.49 22.46 13.49 22.8C13.49 23.14 13.375 23.425 13.145 23.655C12.915 23.885 12.63 24 12.29 24H2.69004ZM18.29 20.91V22.38C18.29 22.72 18.175 23.005 17.945 23.235C17.715 23.465 17.43 23.58 17.09 23.58C16.75 23.58 16.465 23.465 16.235 23.235C16.005 23.005 15.89 22.72 15.89 22.38V18C15.89 17.66 16.005 17.375 16.235 17.145C16.465 16.915 16.75 16.8 17.09 16.8H21.47C21.81 16.8 22.095 16.915 22.325 17.145C22.555 17.375 22.67 17.66 22.67 18C22.67 18.34 22.555 18.625 22.325 18.855C22.095 19.085 21.81 19.2 21.47 19.2H19.97L22.67 21.9C22.89 22.12 23 22.395 23 22.725C23 23.055 22.89 23.34 22.67 23.58C22.43 23.82 22.145 23.94 21.815 23.94C21.485 23.94 21.2 23.82 20.96 23.58L18.29 20.91Z" fill="black" />
+            </svg>
+          </label>
           <div >
             {curso && curso.ARQUIVO && curso.ARQUIVO.endsWith('.pdf') ? (
               <embed id="orgimg" src={RotaBanco + `/Images/${curso.ARQUIVO}`} type="application/pdf" width="100%" height="500px" />
@@ -209,6 +216,12 @@ const EditarCurso = () => {
     const imagemInput = event.target;
     const imagemFile = imagemInput.files[0];
     const orgimg = document.getElementById('orgimg');
+
+    if (imagemFile) {
+      setNomeArq(imagemFile.name);
+    } else {
+      setNomeArq('');
+    }
 
     if (imagemFile) {
       if (imagemFile.type === 'application/pdf') {
