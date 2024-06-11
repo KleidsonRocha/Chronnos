b1221f90a48891fa24cca1fbe3818b39b8396655
Vinicius Alexandre Lucas <viniciusalucas@gmail.com>
2024-06-11 02:55:21 -0300

    Adicionando a animacao da tela de login

--- FrontEnd/src/Components/Login.jsx
+++ FrontEnd/src/Components/Login.jsx
@@ -4,6 +4,8 @@ import ChronnosButton from './inputs-buttons/ChronnosButton/ChronnosButton';
 import MainMobile from './layouts/MainMobile/MainMobile';
 import '../pages/LoginCadastro/styles.css'
 import '../Assets/utility.css';
+import './Login/styles.css';
+import backgroundLogin from '../Assets/background-login.mp4';
 import { useGlobalContext } from '../App';
 
 function Login() {
@@ -14,11 +16,11 @@ function Login() {
   const { RotaBanco } = useGlobalContext();
 
   useEffect(() => {
-    if(email === '' || senha === '') {
+    if (email === '' || senha === '') {
       setBtnLoginClass('button-insatisfaz');
     } else {
       setBtnLoginClass('button-default');
-    }    
+    }
   }, [email, senha]);
 
   const handleLogin = async () => {
@@ -44,15 +46,26 @@ function Login() {
   };
 
   return (
-    <MainMobile className={"form-mob-cent"}>
-      <h1 className="txt-titulo">Chronnos</h1>
-      <p className='txt-instrucao'>Sua carteira digital de cursos e certificados.</p>
-      {error && <p className="txt-error">{error}</p>}
-      <ChronnosInput type="email" id="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} className="input-default"></ChronnosInput>
-      <ChronnosInput type="password" id="senha" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} className="input-default"></ChronnosInput>
-      <ChronnosButton onClick={handleLogin} className={btnLoginClass}>Efetuar o login</ChronnosButton>
-      <p className="txt-footer">Ainda não possui uma conta? Crie <a href="/Cadastro">aqui</a>.</p>
-    </MainMobile>
+    <>
+      <MainMobile className="login">
+        <div className="anim-troca-cor">
+          <h1 className="txt-titulo">Chronnos</h1>
+        </div>
+        <div className="anim-troca-cor">
+          <p className='txt-instrucao'>Sua carteira digital de cursos e certificados.</p>
+        </div>
+        {error && <p className="txt-error">{error}</p>}
+        <ChronnosInput type="email" id="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} className="input-default"></ChronnosInput>
+        <ChronnosInput type="password" id="senha" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} className="input-default"></ChronnosInput>
+        <ChronnosButton onClick={handleLogin} className={btnLoginClass}>Efetuar o login</ChronnosButton>
+        <div className="anim-troca-cor">
+          <p className="txt-footer">Ainda não possui uma conta? Crie <a href="/Cadastro">aqui</a>.</p>
+        </div>
+      </MainMobile>
+      <div className="background-animation">
+        <video autoPlay muted src={backgroundLogin} type="video/mp4" />
+      </div>
+    </>
   );
 }
 
