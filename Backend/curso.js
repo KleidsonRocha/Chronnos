89d1048f5b470af89b162df92ce782dfbef4cb17
Kleidson <kleidsonmr@gmail.com>
2024-06-13 09:22:55 -0300

    ajuste de edição de curso

--- Backend/curso.js
+++ Backend/curso.js
@@ -156,7 +156,6 @@ router.post('/editarCurso', upload.single('imagem'), (req, res) => {
   let imagem;
 
   if (req.file == undefined) {
-    console.log(imagem);
     imagem = "NULL";
   } else {
     imagem = req.file.filename;
